92bf6946fda9efe5ea2d11cb81de789ec2ab25d3
Piouka <nina.batko@gmail.com>
2020-05-06 11:26:16 +0200

    fcts table symboles finies

--- projet/symboltable.c
+++ projet/symboltable.c
@@ -29,20 +29,6 @@ typedef struct s_table {
 // On cree la table et on met tous ses elements a 0
 table global_table = {0};
 
-// Fonction qui rajoute un element dans la table
-void add_table(char *e_id, u_int16_t e_adress, int e_init, int e_type, int e_depth) {
-    // Check if a double definition exists
-    element new_element = global_table.table[global_table.index];
-    element *p_new_element = &new_element;
-    strcpy(p_new_element -> id, e_id);
-    p_new_element -> adress = e_adress;
-    p_new_element -> init = e_init;
-    p_new_element -> type = e_type;
-    p_new_element -> depth = e_depth;
-
-    global_table.index ++;
-}
-
 // Fonction qui trouve le bon element et renvoie un pointeur sur la ligne de la table
 element* get_element(char *id) {
     int index = global_table.index;
@@ -60,6 +46,27 @@ element* get_element(char *id) {
     return element_trouve;
 }
 
+// Fonction qui rajoute un element dans la table
+void add_table(char *e_id, u_int16_t e_adress, int e_init, int e_type, int e_depth) {
+    // Check if a double definition exists
+    element* element_cmp = get_element(e_id);
+    if (element_cmp -> depth == e_depth) {
+        printf("ERREUR: double definition !");
+    }
+    else {
+        element new_element = global_table.table[global_table.index];
+        element *p_new_element = &new_element;
+        strcpy(p_new_element -> id, e_id);
+        p_new_element -> adress = e_adress;
+        p_new_element -> init = e_init;
+        p_new_element -> type = e_type;
+        p_new_element -> depth = e_depth;
+
+        global_table.index ++;
+    }
+    
+}
+
 // Gestion de la profondeur 
 void erase_depth() {
     int index = global_table.index;
@@ -67,5 +74,9 @@ void erase_depth() {
     while (global_table.table[index].depth == act_depth) {
         global_table.index --;
     }
+}
+
+void print_table(table t) {
+    //TODO
 }
 
